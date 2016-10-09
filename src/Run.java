e1955d409db275a60c782f34aba38a701b1ccce2
Angelo S. <boyangel90@hotmail.it>
2016-10-09 14:10:51 +0200

    Code clean

--- src/Run.java
+++ src/Run.java
@@ -2,42 +2,48 @@ import java.util.Scanner;
 
 public class Run {
 
+	private static ClassicCipher c;
+	private static Scanner s;
+	
 	public static void main(String[] args) {
-		MonoSubCipher c;
-		Scanner s = new Scanner(System.in);
-		String key;
-		String ans;
+	    c = new MonoSubCipher();
+		s = new Scanner(System.in);
+		String ans, plainText, cipherText, decodedText;
 
 		do {
-			System.out.println("Vuoi generare casualmente la chiave? (y/n)");
-			do {
-				ans = s.nextLine();
-			} while (!ans.equals("y") && !ans.equals("n"));
-
-			c = new MonoSubCipher();
-			if (ans.equals("y")) {
-				c.setKey(c.genKey());
-			} else {
-				System.out.println("Inserisci la chiave:");
-				do {
-					key = s.nextLine();
-				} while (!c.checkKey(key));
-				c.setKey(key);
-			}
-
+			chooseKey();
 			System.out.println("Inserisci il plain text: ");
-			String plainText = s.nextLine();
-			String cipherText = c.Enc(plainText);
-			String decodedText = c.Dec(cipherText);
-			key = c.getKey();
+			plainText = s.nextLine();
+			cipherText = c.Enc(plainText);
+			decodedText = c.Dec(cipherText);
 
-			System.out.println("PlainText: " + plainText + '\n' + "Key: " + key + '\n' + "CipherText: " + cipherText
+			System.out.println("PlainText: " + plainText + '\n' + "Key: " + c.getKey() + '\n' + "CipherText: " + cipherText
 					+ '\n' + "Decoded Text: " + decodedText);
+			
 			System.out.println("Ripetere l'operazione? (y/n)");
 			ans = s.nextLine();
 		} while (ans.equals("y"));
 
 		s.close();
+	}
+
+	private static void chooseKey() {
+		String ans, key;
+
+		System.out.println("Vuoi generare casualmente la chiave? (y/n)");
+		do {
+			ans = s.nextLine();
+		} while (!ans.equals("y") && !ans.equals("n"));
+
+		if (ans.equals("y")) {
+			c.setKey(c.genKey());
+		} else {
+			System.out.println("Inserisci la chiave:");
+			do {
+				key = s.nextLine();
+			} while (!((MonoSubCipher) c).checkKey(key));
+			c.setKey(key);
+		}
 	}
 
 }
